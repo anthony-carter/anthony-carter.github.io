a2916f4cad8dd7b22521d496fe55cb19eb72a3fd
anthony-carter <anthony@myanswers.io>
2019-12-24 11:29:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let fetchSenators = () => {
- fetch('https://api.propublica.org/congress/v1/116/senate/members.json', {
-  method: 'GET',
-  mode: 'CORS',
-  headers: {
-  'X-API-Key': 'cHWA0cyVrIeIZHFmZyrlp3UBNFO1aqLn7LsYLij2'
-  }
-}).then(response => response.json()) 
+  fetch('https://api.propublica.org/congress/v1/116/senate/members.json', {
+            method: "GET",
+            headers: new Headers({
+                "X-API-Key": 'cHWA0cyVrIeIZHFmZyrlp3UBNFO1aqLn7LsYLij2'
+            })
+  })
+   .then(response => response.json()) 
   .then(response => {
     renderResults(response.results[0].members)
-})}
+  })
+}
