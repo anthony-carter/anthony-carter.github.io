e808514e6e41c8ebbb1b61a7230c5623888f3411
anthony-carter <anthony@myanswers.io>
2019-12-24 11:09:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const url = "https://api.propublica.org/congress/116";
 fetch(url, {
   method: "GET",
-  withCredentials: true,
+  mode: "CORS",
   headers: {
     "X-Auth-Token": "cHWA0cyVrIeIZHFmZyrlp3UBNFO1aqLn7LsYLij2"
   }
 })
-  .then(resp => resp.json())
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-
+.then(response => response.json()) 
+  .then(response => {
+    renderResults(response.results[0].members)
