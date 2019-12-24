2e00bd9bb884d45a4a07a3534a540c3d732aee51
anthony-carter <anthony@myanswers.io>
2019-12-24 10:46:21 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-const app = document.getElementById('root');
+//const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+//const logo = document.createElement('img');
+//logo.src = 'logo.png';
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
+//const container = document.createElement('div');
+//container.setAttribute('class', 'container');
 
-app.appendChild(logo);
-app.appendChild(container);
+//app.appendChild(logo);
+//app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://api.propublica.org/congress/116/', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
+/*  if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
@@ -37,5 +37,5 @@ request.onload = function () {
     app.appendChild(errorMessage);
   }
 }
-
+*/
 request.send();
