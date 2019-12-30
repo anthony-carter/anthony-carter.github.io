89ec0048ecd91ce435c99cfeea30933e2903e33e
anthony-carter <anthony@myanswers.io>
2019-12-30 19:49:02 +0100

    Update main_vue.js

--- main_vue.js
+++ main_vue.js
@@ -18,7 +18,7 @@ var main = new Vue({
                 fetch("https://api.propublica.org/congress/v1/113/senate/members.json", {
                     method: "GET",
                     headers: new Headers({
-                        "X-API-Key": 'adZUIoKPgkk0ecKXE0ztm9ErLNJgARlsKHBhTBYa'
+                        "X-API-Key": 'cHWA0cyVrIeIZHFmZyrlp3UBNFO1aqLn7LsYLij2'
                     })
                 }).then(function (response) {
                     if (response.ok)
